1580b9771646c4d8a9d9fbfc0759ba9b284076b3
Gustavo  Coutinho <guscoutinho@outlook.com>
2019-02-16 09:23:59 -0800

    set up storyboard +view controllers

---
+++ gmj/gmj/Views/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gmj
+//
+//  Created by Gustavo  Coutinho on 2/16/19.
+//  Copyright © 2019 Gustavo  Coutinho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
