96b1ee9416919059e02860eaab4247c32e93305d
Jessica Au <aujk@Jessicas-MacBook-Pro.local>
2019-02-16 11:33:02 -0800

    setup

---
+++ gmj/gmj/Models/Call.m
@@ -0,0 +1,13 @@
+//
+//  Call.m
+//  gmj
+//
+//  Created by Jessica Au  on 2/16/19.
+//  Copyright © 2019 Gustavo  Coutinho. All rights reserved.
+//
+
+#import "Call.h"
+
+@implementation Call
+
+@end
