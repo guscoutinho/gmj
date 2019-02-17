c4128a60c2047bf518efca6a6e59b5f7ee0d988f
Gustavo  Coutinho <guscoutinho@outlook.com>
2019-02-16 17:04:00 -0800

    deleted utilities

--- gmj/gmj/ViewControllers/HomeViewController.m
+++ gmj/gmj/ViewControllers/HomeViewController.m
@@ -8,12 +8,11 @@
 
 #import "HomeViewController.h"
 #import "CallTableViewCell.h"
+#import "JSONAttributedFormatter.h"
 
 
-@interface HomeViewController () <UITableViewDataSource, UITableViewDelegate>
-
-@property (weak, nonatomic) IBOutlet UITableView *tableView;
-//@property (nonatomic, strong) UIRefreshControl *refreshControl;
+@interface HomeViewController ()
+- (IBAction)didTapButton:(id)sender;
 
 @end
 
@@ -22,7 +21,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [super viewWillAppear:YES];
-    [self tableViewSetup];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -30,44 +28,7 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)tableViewSetup {
-    self.tableView.backgroundColor = [UIColor colorWithRed:240.0/255.0 green:240.0/255.0 blue:240.0/255.0 alpha:1.0];
-    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
-    self.tableView.dataSource = self;
-    self.tableView.delegate = self;
-    self.tableView.rowHeight = UITableViewAutomaticDimension;
+
+- (IBAction)didTapButton:(id)sender {
 }
-
-//- (void)refreshControlSetup {
-//    // initialization
-//    self.refreshControl = [[UIRefreshControl alloc] init];
-//
-//    // Programagtic view of dragging and dropping in code
-//    [self.refreshControl addTarget:self action:@selector(fetchUserActs) forControlEvents:UIControlEventValueChanged];
-//
-//    // Nests views into subviews
-//    [self.tableView insertSubview:self.refreshControl atIndex:0];
-//    [self.tableView sendSubviewToBack:self.refreshControl];
-//}
-
-- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
-    
-    CallTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CallTableViewCell"];
-    return cell;
-}
-
-- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 5;
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 @end
